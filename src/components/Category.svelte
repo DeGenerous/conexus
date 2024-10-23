f69e55c5ad474b5dfc0ba1eef92e6614b47f9d8f
Dimon <notawid@gmail.com>
2024-10-23 16:23:51 +0200

    adjusted Layout and removed it from Story

--- src/components/Category.svelte
+++ src/components/Category.svelte
@@ -2,11 +2,9 @@
   import type { DynSectionCategory } from '@lib/conexus';
   import StoryCollection from './StoryCollection.svelte';
 
-  export let section: string;
   export let categories: DynSectionCategory[] | undefined;
 </script>
 
-<p class="title">{section}</p>
 {#if categories}
   {#each categories as category}
     <StoryCollection {category} />
@@ -16,16 +14,6 @@
 {/if}
 
 <style>
-  .title {
-    font-size: 5vw;
-    line-height: 5vw;
-    text-align: center;
-    margin: 3vw auto;
-    color: rgba(51, 226, 230, 0.85);
-    -webkit-text-stroke: 0.03vw #33e2e6;
-    filter: drop-shadow(0 0 1vw rgba(51, 226, 230, 0.5));
-  }
-
   .loading {
     text-align: center;
     font-size: 2vw;
@@ -35,15 +23,6 @@
   }
 
   @media only screen and (max-width: 600px) {
-    .title {
-      position: absolute;
-      width: 100vw;
-      top: 1em;
-      font-size: 1.5em;
-      line-height: 1.5em;
-      margin: 0 auto;
-    }
-
     .loading {
       font-size: 1em;
       line-height: 1em;

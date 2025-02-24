702495b838eaca7dff86a5b2d6bc4dea23821934
Rhaqim <anusiemj@gmail.com>
2025-02-24 16:55:02 +0100

    Google analytics

---
+++ src/components/utils/GoogleAnalytics.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-YSHBY9F7FY';
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'G-YSHBY9F7FY');
+      gtag('consent', 'default', {
+        ad_storage: 'denied',
+        ad_user_data: 'denied',
+        ad_personalization: 'denied',
+        analytics_storage: 'denied',
+      });
+    };
+  });
+</script>

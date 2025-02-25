47e0f2aac2cbf0230eac7efe69e3132f94e3ece6
Dimon <notawid@gmail.com>
2025-02-25 17:53:57 +0100

    removed unused custom storage

--- src/stores/conexus.ts
+++ src/stores/conexus.ts
@@ -6,24 +6,3 @@ export const loading = writable<boolean>(false);
 export const background_music = writable<Nullable<string>>(null);
 export const background_image = writable<Nullable<string>>(null);
 export const fullscreen = writable<boolean>(false);
-
-function createPersistedStore<T>(key: string, initialValue: T) {
-  const isBrowser = typeof window !== 'undefined';
-  const storedValue = isBrowser ? localStorage.getItem(key) : null;
-  const store = writable<T>(
-    storedValue ? JSON.parse(storedValue) : initialValue,
-  );
-
-  if (isBrowser) {
-    store.subscribe((value) => {
-      localStorage.setItem(key, JSON.stringify(value));
-    });
-  }
-
-  return store;
-}
-
-export const continuableStore = createPersistedStore<Nullable<CoNexus>>(
-  'continuable',
-  null,
-);

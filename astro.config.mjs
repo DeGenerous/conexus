69f120c6ae05423d893b2a99cb598fac35510c87
Rhaqim <anusiemj@gmail.com>
2025-03-20 02:56:02 +0100

    alternative caching

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   adapter: vercel(),
   vite: {
     server: {
-      proxy: {
+      proxy: { // remove in production
         '/api': {
           target: import.meta.env.VITE_API_URL,
           changeOrigin: true,
--- scripts/pod.sh
+++ scripts/pod.sh
@@ -8,36 +8,17 @@ POD_NAME=conexus-v1-pod
 # Export evnironment variables
 export $(grep -v '^#' .env | xargs)
 
-# # Build the Docker image
-# podman build -t $APP_NAME ..
+# Build the Docker image
+podman build -t $APP_NAME ..
 
-# # Remove the existing container if it exists
-# podman rm -f $CONTAINER_NAME
-
-# # Remove the existing pod if it exists
-# podman pod rm -f $POD_NAME
-
-# # Create a new pod
-# podman pod create --name $POD_NAME -p 4323:4321
-
-# # Run the container in the pod
-# podman run -d --name $CONTAINER_NAME --pod $POD_NAME $APP_NAME
+# Remove the existing container if it exists
+podman rm -f $CONTAINER_NAME
 
 # Remove the existing pod if it exists
-podman pod rm -f $POD_NAME-service
+podman pod rm -f $POD_NAME
 
-# # create and attach redis container to the pod
-# podman create --restart always \
-# --name="$POD_NAME-cache" \
-# --pod="$POD_NAME" \
-# docker.io/library/redis:alpine \
-# redis-server --appendonly yes
+# Create a new pod
+podman pod create --name $POD_NAME -p 4323:4321
 
-# # create and attach the frontend container to the pod
-# podman create --restart always \
-# --name="$POD_NAME-frontend" \
-# --pod="$POD_NAME" \
-# -p 4323:4321 \
-# $APP_NAME
-
-podman start $POD_NAME-service
+# Run the container in the pod
+podman run -d --name $CONTAINER_NAME --pod $POD_NAME $APP_NAME

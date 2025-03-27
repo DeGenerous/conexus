b8311e6709bea5b60a5666c590dcec001e9b8b17
Rhaqim <anusiemj@gmail.com>
2025-03-27 18:39:34 +0100

    category_order

--- src/admin.env.d.ts
+++ src/admin.env.d.ts
@@ -11,12 +11,12 @@ type CollectionTopic = {
 };
 
 type Collection = {
-  category_id: number;
-  category_name: string;
   section_id: number;
   section_name: string;
+  category_id: number;
+  category_name: string;
+  category_order: number;
   topics: CollectionTopic[];
-  order: number;
 };
 
 type MediaType =

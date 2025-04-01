a730180b4a3a99436a9bb5cd4f2e0f2c2a06b003
Dimon <notawid@gmail.com>
2025-04-01 15:34:15 +0200

    highlighted gated stories

--- src/components/utils/StoryTile.svelte
+++ src/components/utils/StoryTile.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import MediaManager from '@lib/media';
   import { CoNexusApp } from '@lib/view';
+  import { onMount } from 'svelte';
 
   export let section: string;
   export let topicID: number;
@@ -13,10 +14,16 @@
     topicName.charAt(0).toUpperCase() + topicName.slice(1)
   ).trim();
 
+  let topicGatings: TopicNFTGate[] = [];
+  onMount(async () => {
+    topicGatings = await view.fetchTopicGates(topicID);
+    console.log(topicGatings)
+  })
+
   const blankPicture = '/blank.avif';
 </script>
 
-<a class="tile" href="/{section}/{topicName}?id={topicID}">
+<a class="tile" class:gated-story={topicGatings.length > 0} href="/{section}/{topicName}?id={topicID}">
   {#await media.fetchStoryImage(topicID, 'tile')}
     <img
       class="tile-picture"
@@ -39,4 +46,53 @@
     />
   {/await}
   <p class="tile-title">{storyName}</p>
+  {#if topicGatings.length > 0}
+    <div class="gatings">
+      <img class="gating-icon" src="/icons/lock.svg" alt="Restricted" />
+      <h3>{topicGatings.map((gate) => (gate.contract_name)).join(', ')}</h3>
+    </div>
+  {/if}
 </a>
+
+<style>
+  .gatings {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1vw;
+    width: 95%;
+    border-radius: 1vw;
+    background-color: rgba(32, 0, 1, 0.5);
+    margin-bottom: 0.5vw;
+    padding: 1vw;
+  }
+
+  .gating-icon {
+    width: 2vw;
+    height: 2vw;
+  }
+
+  h3 {
+    width: 100%;
+    color: rgb(255, 165, 40);
+  }
+
+  @media only screen and (max-width: 600px) {
+    .gatings {
+      gap: 1em;
+      border-radius: 0.75em;
+      margin-bottom: 0.25em;
+      padding: 0.5em;
+    }
+
+    .gating-icon {
+      width: 0.85em;
+      height: 0.85em;
+    }
+
+    h3 {
+      font-size: 0.85em;
+    }
+  }
+</style>

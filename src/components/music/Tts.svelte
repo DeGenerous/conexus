101bcbfe3ad61732196fd5cad83f534bbed7f06e
Dimon <notawid@gmail.com>
2025-04-06 16:45:11 +0200

    restructured background music and tts

--- src/components/music/Tts.svelte
+++ src/components/music/Tts.svelte
@@ -1,40 +1,32 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-
   import { story } from '@stores/conexus';
   import { tts_volume } from '@stores/volumes';
 
   let audio: HTMLAudioElement;
+
   let last_tts: Blob | null = null;
+  let tts: Blob | null = null;
 
-  onMount(() => {
-    let tts: Blob | null = null;
-    story.subscribe((story) => {
-      const step = story?.step_data as StepData;
+  $: step = $story?.step_data as StepData;
 
-      if (step && step.tts && last_tts !== step.tts) {
-        audio.src = window.URL.createObjectURL(step.tts);
-        audio.play();
+  $: if (step && step.tts && last_tts !== step.tts) {
+    audio.src = window.URL.createObjectURL(step.tts);
+    audio.play();
+    tts = step.tts;
+    last_tts = step.tts;
+  }
 
-        tts = step.tts;
-        last_tts = step.tts;
-      }
-    });
+  $: if (audio && $tts_volume) setVolume();
 
-    tts_volume.subscribe(({ muted, volume, restart }) => {
-      if (restart) {
-        if (tts) {
-          audio.src = window.URL.createObjectURL(tts);
-          audio.play();
-        }
+  $: if ($tts_volume.restart) {
+    if (tts) {
+      audio.src = window.URL.createObjectURL(tts);
+      audio.play();
+    }
+    $tts_volume.restart = false;
+  }
 
-        tts_volume.set({ muted, volume, restart: false });
-        return;
-      }
-
-      audio.volume = !muted ? volume : 0;
-    });
-  });
+  const setVolume = () => (audio.volume = $tts_volume.muted ? 0 : $tts_volume.volume);
 </script>
 
 <audio bind:this={audio}></audio>

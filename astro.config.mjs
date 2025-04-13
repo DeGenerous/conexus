c4a15b2a6c7aa578471c11c85219f174f2ac84f5
Rhaqim <anusiemj@gmail.com>
2025-04-13 12:40:36 +0100

    node env

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,55 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig(({
   integrations: [svelte(), react()],
   output: 'server',
   adapter: vercel(),
+
   vite: {
-    server: {
-      
-      allowedHosts: ['conexus.degenerousdao.com', 'conexus-test.degenerousdao.com'],
-    },
+    ...(import.meta.env.NODE_ENV === 'development' && {
+      server: {
+        proxy: {
+          '/api': {
+            target: import.meta.env.VITE_API_URL,
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        },
+        hmr: {
+          host: '.degenerousdao.com',
+          port: 443,
+          protocol: 'wss',
+        },
+        allowedHosts: ['conexus.degenerousdao.com', 'conexus-test.degenerousdao.com'],
+      },
+    }),
   },
+
   devToolbar: {
     enabled: false,
   },
-});
+}));
+// export default defineConfig({
+//   integrations: [svelte(), react()],
+//   output: 'server',
+//   adapter: vercel(),
+//   vite: {
+//     server: {
+//       proxy: {
+//         // remove in production
+//         '/api': {
+//           target: import.meta.env.VITE_API_URL,
+//           changeOrigin: true,
+//           secure: false,
+//           rewrite: (path) => path.replace(/^\/api/, ''),
+//         },
+//       },
+//       allowedHosts: ['conexus.degenerousdao.com', 'conexus-test.degenerousdao.com'],
+//     },
+//   },
+//   devToolbar: {
+//     enabled: false,
+//   },
+// });

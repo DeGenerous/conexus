02c443678f4b3e5b31c5a2e303d5ba1a7319b1dd
Rhaqim <anusiemj@gmail.com>
2025-04-16 18:15:09 +0100

    remove category first

--- src/components/dashboard/dream/manage/Categories.svelte
+++ src/components/dashboard/dream/manage/Categories.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { CATEGORY_CACHE_KEY } from '@constants/cache';
   import { AdminApp } from '@lib/admin';
   import { onMount } from 'svelte';
 
@@ -14,6 +15,9 @@
   const createNewCategory = async () => {
     await admin.newCategory(newCategoryName)
       .then(async () => {
+        // delete categories from localstorage
+        localStorage.removeItem(CATEGORY_CACHE_KEY);
+        // fetch categories again
         categories = await admin.fetchCategories();
         newCategoryName = '';
       });

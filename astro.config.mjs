0f8bbd2f4dfbc81649af8f44f157e01af99c1628
Rhaqim <anusiemj@gmail.com>
2025-04-23 15:24:31 +0100

    SEO handling and manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,42 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import compress from 'astro-compress';
+import { VitePWA } from 'vite-plugin-pwa';
+
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import svelte from '@astrojs/svelte';
 import vercel from '@astrojs/vercel';
+
+import { manifest, seoConfig } from './utils/seoConfig';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), react()],
+  site: seoConfig.baseURL,
+  integrations: [svelte(), react(), sitemap(), compress()],
   output: 'server',
   adapter: vercel(),
   vite: {
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true,
+          type: 'module',
+          navigateFallback: '/',
+        },
+        manifest,
+        workbox: {
+          globDirectory: 'dist',
+          globPatterns: [
+            '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+          ],
+          // Don't fallback on document based (e.g. `/some-page`) requests
+          // This removes an errant console.log message from showing up.
+          // navigateFallback: null,
+          navigateFallback: '/',
+        },
+      }),
+    ],
     server: {
       proxy: {
         // remove in production

c505221758e6f68a440fb3ba4c42d10436ccce20
Dimon <notawid@gmail.com>
2025-04-26 10:46:34 +0200

    added screenshots to the manigest

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -27,7 +27,7 @@ export const seoConfig = {
  * Defines the configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
-  name: 'CoNexus - Degenerous DAO',
+  name: 'CoNexus',
   short_name: 'CoNexus',
   description:
     'A storytelling platform powered by AI and Web3, where your choices shape the multiverse.',
@@ -39,6 +39,64 @@ export const manifest: Partial<ManifestOptions> = {
   orientation: 'any',
   prefer_related_applications: false,
   categories: ['games', 'entertainment', 'education'],
+  screenshots: [
+    {
+      src: "screenshots/pc-1.webp",
+      sizes: "2880x1800",
+      type: "image/webp",
+      form_factor: "wide",
+      label: "Home screen"
+    },
+    {
+      src: "screenshots/pc-2.webp",
+      sizes: "2880x1800",
+      type: "image/webp",
+      form_factor: "wide",
+      label: "Section with stories"
+    },
+    {
+      src: "screenshots/pc-3.webp",
+      sizes: "2880x1800",
+      type: "image/webp",
+      form_factor: "wide",
+      label: "Story page"
+    },
+    {
+      src: "screenshots/pc-4.webp",
+      sizes: "2880x1800",
+      type: "image/webp",
+      form_factor: "wide",
+      label: "Gameplay"
+    },
+    {
+      src: "screenshots/mobile-1.webp",
+      sizes: "591x1131",
+      type: "image/webp",
+      form_factor: "narrow",
+      label: "Home screen"
+    },
+    {
+      src: "screenshots/mobile-2.webp",
+      sizes: "591x1131",
+      type: "image/webp",
+      form_factor: "narrow",
+      label: "Section with stories"
+    },
+    {
+      src: "screenshots/mobile-3.webp",
+      sizes: "591x1131",
+      type: "image/webp",
+      form_factor: "narrow",
+      label: "Story page"
+    },
+    {
+      src: "screenshots/mobile-4.webp",
+      sizes: "591x1131",
+      type: "image/webp",
+      form_factor: "narrow",
+      label: "Gameplay"
+    }
+  ],
   icons: [
     {
       src: '/icons/icon-192.png',
@@ -49,12 +107,6 @@ export const manifest: Partial<ManifestOptions> = {
       src: '/icons/icon-512.png',
       sizes: '512x512',
       type: 'image/png',
-    },
-    {
-      src: '/icons/icon-512.png',
-      sizes: '512x512',
-      type: 'image/png',
-      purpose: 'any maskable',
-    },
+    }
   ],
 };

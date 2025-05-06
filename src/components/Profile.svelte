7d7d227102ca0e5dbba2d5d47c0c936219096b08
Dimon <notawid@gmail.com>
2025-05-06 14:24:46 +0200

    dont render 'Add wallet' button if there is no web2 account

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -552,12 +552,14 @@
                       </li>
                     {/each}
                   </ul>
-                  <div class="buttons-container">
-                    <WalletConnect
-                      linking={true}
-                      title={'Add another address'}
-                    />
-                  </div>
+                  {#if user?.email && user?.first_name}
+                    <div class="buttons-container">
+                      <WalletConnect
+                        linking={true}
+                        title={'Add another address'}
+                      />
+                    </div>
+                  {/if}
                 </div>
 
                 {#if $accountError && $accountError.selectMainWallet}

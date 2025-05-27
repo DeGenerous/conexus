734627c8d133539fcba0c12520b0d767954721ee
Dimon <notawid@gmail.com>
2025-05-27 10:58:43 +0200

    styled a cookies banner

--- src/components/utils/CookieBanner.svelte
+++ src/components/utils/CookieBanner.svelte
@@ -63,66 +63,28 @@
 </script>
 
 {#if showBanner}
-  <div class="cookie-banner blur">
-    <h3>
+  <div class="container">
+    <h5>
       We use cookies to provide you with a better service. By continuing to use
       this website, you consent to the use of cookies as described in our
       <a
         href="https://docs.google.com/document/d/1kkIY-86y2LtoM4IXzp80E5H7Op1YSezw8nPBG1AQ2uo/edit?usp=sharing"
         target="_blank">Privacy Policy</a
       >.
-    </h3>
-    <div>
-      <button on:click={() => acceptCookies(true)}>Accept all</button>
+    </h5>
+    <div class="flex-row">
+      <button class="green-btn" on:click={() => acceptCookies(true)}>Accept all</button>
       <button on:click={() => acceptCookies(false)}>Essential only</button>
     </div>
   </div>
 {/if}
 
 <style>
-  .cookie-banner {
+  .container {
     position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100vw;
-    text-align: center;
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: center;
-    align-items: center;
-    gap: 1vw;
-    padding: 1.5vw;
-    background-color: rgba(1, 0, 32, 0.75);
-    box-shadow: 0 0 0.5vw #010020;
-    z-index: 1000;
-  }
-
-  h3 {
-    width: 75%;
-    line-height: 2.5vw;
-  }
-
-  .cookie-banner div {
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: center;
-    align-items: center;
-    gap: 1.5vw;
-  }
-
-  @media only screen and (max-width: 600px) {
-    .cookie-banner {
-      gap: 1em;
-      padding: 1.5em;
-    }
-
-    h3 {
-      width: 100%;
-      line-height: 1.5em;
-    }
-
-    .cookie-banner div {
-      gap: 1em;
-    }
+    bottom: 1rem;
+    width: 75rem;
+    left: 50%;
+    transform: translateX(-50%);
   }
 </style>

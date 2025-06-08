2db1873bfa7cf832cb1b909555864b5dc53976ae
Dimon <notawid@gmail.com>
2025-06-08 15:11:40 +0200

    restyled the general Dream page and defined styling for dropareas

--- src/components/icons/Close.svelte
+++ src/components/icons/Close.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
-  let { onclick = () => {} }: { onclick: () => void } = $props();
+  let {
+    onclick = () => {},
+    voidBtn = false
+  }: {
+    onclick: () => void;
+    voidBtn?: boolean;
+  } = $props();
 </script>
 
-<button {onclick} aria-label="Close">
+<button
+  class="flex"
+  class:void-btn={voidBtn}
+  {onclick}
+  aria-label="Close"
+>
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="-100 -100 200 200"
     stroke-width="30"
     stroke-linecap="round"
+    style:width={voidBtn ? '1.75rem' : ''}
   >
     <path d="M -65 -65 L 65 65 M -65 65 L 65 -65" fill="none" />
   </svg>

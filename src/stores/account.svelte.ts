90b53e73702fa211d1fdd8b31be94e534bc6f7ea
Rhaqim <anusiemj@gmail.com>
2025-08-30 23:53:23 +0100

    update the get referral code

--- src/stores/account.svelte.ts
+++ src/stores/account.svelte.ts
@@ -2,10 +2,9 @@ import { writable } from 'svelte/store';
 
 export const authenticated = writable<Nullable<User>>(null);
 
-export const referralCodes = writable<ReferralCode[]>([]);
+export const referralCodes = writable<ReferralCode | null>(null);
 
 export const accountError = writable<AccountError>(null);
-
 
 export const isAdmin = writable(false);
 export const isCreator = writable(false);

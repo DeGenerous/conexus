f4aa053608b561b9c1a74631a9cd57627341bbdc
Rhaqim <anusiemj@gmail.com>
2025-09-13 05:06:58 +0100

    updating routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,14 +1,16 @@
 export const NAV_ROUTES = {
-  WEBSITE: 'https://degenerousdao.com/',
-  TERMS: 'https://degenerousdao.com/legal/terms-of-service',
-  SUPPORT: 'mailto:support@degenerousdao.com',
+  CONEXUS: 'https://conexus.ink',
+  WEBSITE: 'https://dgrslabs.ink/',
+  TERMS: 'https://dgrslabs.ink/legal/terms-of-service',
+  SUPPORT: 'mailto:support@dgrslabs.ink',
   WIKI: 'https://degenerousdao.gitbook.io/wiki',
-  DISCORD: 'https://degenerousdao.com/join',
+  DISCORD: 'https://dgrslabs.ink/join',
   TWITTER: (encodedMessage: string, encodedURI: string) =>
     `https://twitter.com/intent/tweet?text=${encodedMessage}&url=${encodedURI}`,
   FACEBOOK: (encodedMessage: string, encodedURI: string) =>
     `https://www.facebook.com/sharer/sharer.php?u=${encodedURI}&quote=${encodedMessage}`,
 
+  // TODO: Deprecate these routes
   HOME: '/dashboard',
   DREAM: '/dashboard/dream',
   CREATE: '/dashboard/dream/create',

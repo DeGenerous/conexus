22f3f3b723b3914901193bfdf664b9bbad1b8234
Dimon <notawid@gmail.com>
2025-10-05 14:16:37 +0200

    adjusted Manage Categories styling

--- src/components/dashboard/dream/manage/Category.svelte
+++ src/components/dashboard/dream/manage/Category.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
   import CategoryView from '@lib/category';
-
-  import CategoryFetcher from '@components/dashboard/common/CategoryFetcher.svelte';
-
   import { ensureCreator } from '@utils/route-guard';
+  
+  import CategoryFetcher from '@components/dashboard/common/CategoryFetcher.svelte';
 
   let categoryView = new CategoryView();
 
@@ -22,6 +21,10 @@
   let newCategoryName = $state<string>('');
   let newCategoryDescription = $state<string>('');
   let newCategorySortOrder = $state<number>(0);
+
+  $effect(() => {
+    if (newCategorySortOrder < 0) newCategorySortOrder = 0;
+  });
 
   let addingCategory = $state(false);
 
@@ -118,42 +121,40 @@
 </section>
 
 <div class="container">
-  <div class="category-inputs">
-    <div>
-      <label for="new-category-name">New Category Name:</label>
-      <input
-        id="new-category-name"
-        bind:value={newCategoryName}
-        placeholder="Enter New Category Name"
-      />
-    </div>
-    <div>
-      <label for="new-category-description">New Category Description:</label>
-      <input
-        id="new-category-description"
-        bind:value={newCategoryDescription}
-        placeholder="Enter New Category Description"
-      />
-    </div>
-    <div>
-      <label for="new-category-sort-order">New Category Sort Order:</label>
-      <input
-        id="new-category-sort-order"
-        type="number"
-        bind:value={newCategorySortOrder}
-        placeholder="Enter New Category Sort Order"
-      />
-    </div>
-  </div>
+  <span class="input-container">
+    <label for="new-category-name">Name</label>
+    <input
+      id="new-category-name"
+      bind:value={newCategoryName}
+      placeholder="Enter New Category Name"
+    />
+  </span>
+  <span class="input-container">
+    <label for="new-category-description">Description</label>
+    <input
+      id="new-category-description"
+      bind:value={newCategoryDescription}
+      placeholder="Enter New Category Description"
+    />
+  </span>
+  <span class="input-container">
+    <label for="new-category-sort-order">Sort Order</label>
+    <input
+      id="new-category-sort-order"
+      type="number"
+      bind:value={newCategorySortOrder}
+      placeholder="Enter New Category Sort Order"
+    />
+  </span>
+
   <button
-    class="green-btn"
     onclick={addCategory}
     disabled={!newCategoryName || addingCategory}
   >
     {#if addingCategory}
       Adding...
     {:else}
-      Add Category
+      Add New Category
     {/if}
   </button>
 </div>
@@ -161,38 +162,8 @@
 <style lang="scss">
   @use '/src/styles/mixins' as *;
 
+  .container,
   .dream-container {
-    .container {
-      flex-wrap: wrap;
-      justify-content: center;
-
-      .category {
-        @include gray(0.25);
-
-        &:hover,
-        &:active {
-          @include cyan(1, text);
-          @include light-blue(0.5);
-        }
-      }
-    }
-  }
-
-  .category-inputs {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-
-    label {
-      font-weight: bold;
-    }
-
-    input {
-      width: 100%;
-    }
-
-    @include respond-up(tablet) {
-      flex-direction: row;
-    }
+    width: 100%;
   }
 </style>

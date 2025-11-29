3a401c22ffc7b8acb4d2ed9691796c744a77da46
Dimon <notawid@gmail.com>
2025-11-29 19:45:54 +0100

    moved switching arrows to separate component

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import { onMount } from 'svelte';
 
-  import CoNexusApp from '@lib/view';
   import { trailerURL } from '@constants/media';
   import { story, game } from '@stores/conexus.svelte';
-  import { prevItem, nextItem } from '@stores/navigation.svelte';
   import {
     getCurrentUser,
     checkUserRoles,
@@ -56,19 +54,9 @@
   // Get user and roles on every page load
   onMount(initializeUser);
 
-  const app: CoNexusApp = new CoNexusApp();
   let hiddenHeader = $state<boolean>(false);
   let showIntro = $state<boolean>(false);
   let onboarding = $state<boolean>(false);
-
-  const navigateTo = (item: Nullable<NavItem>) => {
-    if (!item) return;
-    if (item.link) {
-      window.location.href = item.link; // or `location.href = item.link`
-    } else if (item.action) {
-      item.action();
-    }
-  };
 
   // FULLSCREEN
 
@@ -122,63 +110,8 @@
       {#if header === 'CoNexus'}
         <h1 class="sr-only">CoNexus</h1>
         <ConexusLogo />
-      {:else if header}
-        {#if $prevItem}
-          <button
-            class="flex-row fade-in"
-            class:inactive={!$prevItem}
-            onclick={() => navigateTo($prevItem)}
-            disabled={!$prevItem}
-            draggable="false"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="-100 -100 200 200"
-              fill="white"
-            >
-              <polygon
-                points="-75 0 -10 -65 -10 65"
-                stroke-width="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <rect x="-30" y="-25" width="100" height="50" rx="5" />
-            </svg>
-            <p>{$prevItem?.name}</p>
-          </button>
-        {/if}
-        {#if !$prevItem && $nextItem}
-          <button style:visibility="hidden">Placeholder</button>
-        {/if}
+      {:else}
         <h1>{header}</h1>
-        {#if $nextItem}
-          <button
-            class="flex-row fade-in"
-            class:inactive={!$nextItem}
-            onclick={() => navigateTo($nextItem)}
-            disabled={!$nextItem}
-            draggable="false"
-          >
-            <p>{$nextItem?.name}</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="-100 -100 200 200"
-              fill="white"
-              style:transform="rotate(180deg)"
-            >
-              <polygon
-                points="-75 0 -10 -65 -10 65"
-                stroke-width="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <rect x="-30" y="-25" width="100" height="50" rx="5" />
-            </svg>
-          </button>
-        {/if}
-        {#if $prevItem && !$nextItem}
-          <button style:visibility="hidden">Placeholder</button>
-        {/if}
       {/if}
     </header>
   {/if}
@@ -245,30 +178,6 @@
 
       h1 {
         width: 100%;
-      }
-
-      button {
-        fill: currentColor;
-        justify-content: space-between;
-
-        p {
-          display: none;
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-        }
-      }
-
-      @include respond-up(tablet) {
-        align-items: flex-start;
-
-        button {
-          width: 12.5rem;
-
-          p {
-            display: block;
-          }
-        }
       }
     }
 

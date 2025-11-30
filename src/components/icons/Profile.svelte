ed8b90f754f4d5e5a0c709b11bd78a357abae855
Dimon <notawid@gmail.com>
2025-11-30 18:50:42 +0100

    hide omnihub for non web3 users

--- src/components/icons/Profile.svelte
+++ src/components/icons/Profile.svelte
@@ -185,16 +185,18 @@
     >
       Settings
     </a>
-    <a
-      class="nohover-link"
-      href="/dashboard#/omnihub"
-      onclick={(event) => {
-        event.preventDefault();
-        redirectTo('/dashboard#/omnihub');
-      }}
-    >
-      OmniHub
-    </a>
+    {#if $user?.wallets?.filter((wallet) => !wallet.faux).length}
+      <a
+        class="nohover-link"
+        href="/dashboard#/omnihub"
+        onclick={(event) => {
+          event.preventDefault();
+          redirectTo('/dashboard#/omnihub');
+        }}
+      >
+        OmniHub
+      </a>
+    {/if}
     <a
       class="nohover-link"
       href="/"
